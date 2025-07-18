1e2ea3d21fb52caa1fa97da3176793ae734eec15
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2025-07-18 16:04:41 +0530

    Test commit 22

---
+++ folder2/test.py
@@ -0,0 +1,4 @@
+import logging
+
+logging.info("hello world")
+logging.info("Byee world")
--- test2.py
+++ test2.py
@@ -2,3 +2,7 @@ import logging
 
 logging.debug("hello world")
 logging.debug("Byee world")
+
+logging.info("Some world")
+
+logging.debug("Another debug logging.info('msg') message")
