c31e2d1e61d7592604cf744193ad60887983a399
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2025-07-21 11:53:03 +0530

    Test commit 30

--- test2.py
+++ test2.py
@@ -2,3 +2,16 @@ import logging
 
 logging.debug("hello world")
 logging.debug("Byee world")
+
+class Test:
+    def test_logging(self):
+        logging.info("This is a test log")
+        assert True
+        
+    def test_another_logging(self):
+        logging.info("Another test log")
+        assert True
+        
+    def test_no_logging(self):
+        assert True
+        
